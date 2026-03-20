9266a1a4f78db21912113da706bbf90d87fb8a13
Baumzin <brenosuvale@gmail.com>
2026-03-20 09:21:28 -0300

    feat - Inserção do campo phone

--- src/components/handleAddEdit.js
+++ src/components/handleAddEdit.js
@@ -1,7 +1,7 @@
 import { Alert } from "react-native";
 import { addPerson, editPerson } from '../servers/personCrud.js';
 
-const handleAddEdit = ({ firstname, lastname, email, route, navigation }) => {
+const handleAddEdit = ({ firstname, lastname, email, phone, route, navigation }) => {
     const person = route.params?.person;
 
     //deixo como arrow function para não ser executada automaticamente 
@@ -10,20 +10,20 @@ const handleAddEdit = ({ firstname, lastname, email, route, navigation }) => {
     const AdviseNullFields = () => Alert.alert("Error", "Please fill in all fields to proceed.", [{ text: "OK" }]);
 
     person ?
-        Alert.alert("Edit Person", `Value of the fields will be updated to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}`,
+        Alert.alert("Edit Person", `Value of the fields will be updated to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}\nPhone: ${phone}`,
             [{ text: "Cancel" },
             {
                 text: "Save", onPress: () => {
-                    editPerson(person.id, { firstname, lastname, email });
+                    editPerson(person.id, { firstname, lastname, email, phone });
                     navigation.navigate("Home");
                 }
             }])
-        : (!firstname || !lastname || !email) ? AdviseNullFields()
-            : Alert.alert("Add Person", `Value of the fields will be added to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}`,
+        : (!firstname || !lastname || !email || !phone) ? AdviseNullFields()
+            : Alert.alert("Add Person", `Value of the fields will be added to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}\nPhone: ${phone}`,
                 [{ text: "Cancel" },
                 {
                     text: "Save", onPress: () => {
-                        addPerson({ firstname, lastname, email });
+                        addPerson({ firstname, lastname, email, phone });
                         navigation.navigate("Home");
                     }
                 }])
