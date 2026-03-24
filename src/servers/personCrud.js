c4b76c035c52576f8c90d3fba1d208b0d5e5eb1e
BRENO VALENTIM BERNARDO <breno.bernardo01@etec.sp.gov.br>
2026-03-24 14:16:28 -0300

    fix - Troca de endpoint people para person

--- src/servers/personCrud.js
+++ src/servers/personCrud.js
@@ -1,19 +1,21 @@
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export async function addPerson(person) {
-    return fetch(`${API_URL}/people`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'ngrok-skip-browser-warning': 'true'
-        },
-        body: JSON.stringify(person),
-    })
+    return (
+        await fetch(`${API_URL}/person`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'ngrok-skip-browser-warning': 'true'
+            },
+            body: JSON.stringify(person),
+        })
+    )
 }
 
 export async function deletePerson(id) {
     return (
-        fetch(`${API_URL}/people/${id}`, {
+        await fetch(`${API_URL}/person/${id}`, {
             headers: {
                 'ngrok-skip-browser-warning': 'true'
             },
@@ -23,12 +25,12 @@ export async function deletePerson(id) {
 
 export async function editPerson(id, person) {
     return (
-        fetch(`${API_URL}/people/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'ngrok-skip-browser-warning': 'true'
-        },
-        body: JSON.stringify(person),
-    }))
+        await fetch(`${API_URL}/person/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'ngrok-skip-browser-warning': 'true'
+            },
+            body: JSON.stringify(person),
+        }))
 }
