a3abb354640bbe5da9ddc4162e0896d534d684ef
Baumzin <brenosuvale@gmail.com>
2026-03-29 14:17:12 -0300

    updt - Mudança na lógica de refreshList + feat - Implementação do componente Loading

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { useState, useCallback } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import Button from '../components/Button';
 import Search from '../components/Search.js';
+import Loading from '../components/Loading.js';
 import styles from '../styles/styles';
 import buttonStyles from '../styles/button.styles';
 import { deletePerson } from '../servers/personCrud.js';
@@ -12,6 +13,7 @@ const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export default function HomeScreen({ navigation }) {
     const [person, setPerson] = useState([]);
+    const [loading, setLoading] = useState(true);
     // O useEffect foi substituido pelo useFocusEffect para atualizar a lista toda vez que haver uma navegação entre telas, ou seja, quando a tela for focada. 
     // O useEffect só atualizava a lista quando a tela era carregada pela primeira vez.
     useFocusEffect(
@@ -21,42 +23,62 @@ export default function HomeScreen({ navigation }) {
     );
 
     async function refreshList() {
-        await fetch(`${API_URL}/person`)
-            .then((response) => response.json())
-            .then((data) => setPerson(data))
-            .catch((error) => console.error(error));
+        try {
+            setLoading(true);
+
+            const response = await fetch(`${API_URL}/people`);
+            const data = await response.json();
+
+            setPerson(data);
+        }
+        catch (error) {
+            alert("Error fetching people. Please try again.");
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
-        <View style={styles.container}>
-            <Search data={person} renderItem={({ item }) => (
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>{item.firstname} {item.lastname}</Text>
-                    <Text style={styles.text}>{item.email}</Text>
-                    <Text style={styles.text}>{item.phone}</Text>
-                    <View style={buttonStyles.buttonContainer}>
-                        <Button
-                            title="Delete Person" onPress={() => {
-                                Alert.alert("Delete Person", "Are you sure you want to delete this person?",
-                                    [{ text: "Cancel" },
-                                    {
-                                        text: "Delete", onPress: () => {
-                                            deletePerson(item.id).then(() => {
-                                                refreshList()
-                                            })
-                                        }
-                                    }])
-                            }}
-                            backgroundColor={"#f07d7d"} />
-                        <Button title="Edit Person" onPress={() => navigation.navigate('AddEdit', { person: item })} backgroundColor={"#b3b477"} />
-                    </View>
-                </View>
-            )} />
-            <View style={buttonStyles.buttonContainer}>
-                <View style={buttonStyles.containerAdd}>
-                    <Button title="Add Person" onPress={() => navigation.navigate('AddEdit')} backgroundColor={"#79b477"}/>
-                </View>
+        <>
+            <View style={styles.container}>
+                {
+                    loading ? (
+                        <Loading />
+                    ) : (
+                        <>
+                            <Search data={person} renderItem={({ item }) => (
+                                <View style={styles.textContainer}>
+                                    <Text style={styles.text}>{item.firstname} {item.lastname}</Text>
+                                    <Text style={styles.text}>{item.email}</Text>
+                                    <Text style={styles.text}>{item.phone}</Text>
+                                    <View style={buttonStyles.buttonContainer}>
+                                        <Button
+                                            title="Delete Person" onPress={() => {
+                                                Alert.alert("Delete Person", "Are you sure you want to delete this person?",
+                                                    [{ text: "Cancel" },
+                                                    {
+                                                        text: "Delete", onPress: () => {
+                                                            deletePerson(item.id).then(() => {
+                                                                refreshList()
+                                                            })
+                                                        }
+                                                    }])
+                                            }}
+                                            backgroundColor={"#f07d7d"} />
+                                        <Button title="Edit Person" onPress={() => navigation.navigate('AddEdit', { person: item })} backgroundColor={"#b3b477"} />
+                                    </View>
+                                </View>
+                            )} />
+                            <View style={buttonStyles.buttonContainer}>
+                                <View style={buttonStyles.containerAdd}>
+                                    <Button title="Add Person" onPress={() => navigation.navigate('AddEdit')} backgroundColor={"#79b477"} />
+                                </View>
+                            </View>
+                        </>
+                    )
+                }
             </View>
-        </View>
+        </>
     );
 }
