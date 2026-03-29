87d55e93f1c226c7748b0b327a30c0617c8a8542
Baumzin <brenosuvale@gmail.com>
2026-03-29 14:17:37 -0300

    feat- Adição de tratativa de erros com try/catch

--- src/servers/personCrud.js
+++ src/servers/personCrud.js
@@ -1,36 +1,48 @@
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export async function addPerson(person) {
-    return (
-        await fetch(`${API_URL}/person`, {
+    try {
+        const response = await fetch(`${API_URL}/people`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'ngrok-skip-browser-warning': 'true'
             },
             body: JSON.stringify(person),
-        })
-    )
+        });
+
+        return response;
+    } catch (error) {
+        console.error("Error adding person:", error);
+    }
 }
 
 export async function deletePerson(id) {
-    return (
-        await fetch(`${API_URL}/person/${id}`, {
+    try {
+        const response = await fetch(`${API_URL}/people/${id}`, {
             headers: {
                 'ngrok-skip-browser-warning': 'true'
             },
             method: 'DELETE',
-        }))
+        });
+        return response;
+    } catch (error) {
+        console.error("Error deleting person:", error);
+    }
 }
 
 export async function editPerson(id, person) {
-    return (
-        await fetch(`${API_URL}/person/${id}`, {
+    try {
+        const response = await fetch(`${API_URL}/people/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
                 'ngrok-skip-browser-warning': 'true'
             },
             body: JSON.stringify(person),
-        }))
+        });
+        return response;
+    } catch (error) {
+        console.error("Error editing person:", error);
+    }
 }
