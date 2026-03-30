ea168f7c4b65dfbf97bc33221bede15e9c1abb38
Baumzin <brenosuvale@gmail.com>
2026-03-30 08:50:19 -0300

    feat - Melhoria na verificação de campos vazios

--- src/components/handleAddEdit.js
+++ src/components/handleAddEdit.js
@@ -6,27 +6,46 @@ const handleAddEdit = ({ firstname, lastname, email, phone, route, navigation })
 
     //deixo como arrow function para não ser executada automaticamente 
     //quando handleAddEdit for chamado, apenas se os campos estiverem vazios.
-    
-    const AdviseNullFields = () => Alert.alert("Error", "Please fill in all fields to proceed.", [{ text: "OK" }]);
+
+    function AdviseNullField ({ firstname, lastname, email, phone }) {
+        let emptyFields = [];
+        
+        if (!firstname?.trim()) emptyFields.push("First Name");
+        if (!lastname?.trim()) emptyFields.push("Last Name");
+        if (!email?.trim()) emptyFields.push("Email");
+        if (!phone?.trim()) emptyFields.push("Phone");
+
+        if (emptyFields.length > 0) {
+            Alert.alert("Error", ` The Following fields are empty:\n\n${emptyFields.join("\n")}.\n\n Please fill them to proceed.`, [{ text: "OK" }]);
+        }
+
+        return emptyFields;
+    };
+
+    if(!person) {
+        const emptyFields = AdviseNullField({ firstname, lastname, email, phone });
+        if (emptyFields.length > 0) return;
+    }
 
     person ?
         Alert.alert("Edit Person", `Value of the fields will be updated to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}\nPhone: ${phone}`,
             [{ text: "Cancel" },
             {
-                text: "Save", onPress: () => {
-                    editPerson(person.id, { firstname, lastname, email, phone });
+                text: "Save", onPress: async () => {
+                    await editPerson(person.id, { firstname, lastname, email, phone });
                     navigation.navigate("Home");
                 }
             }])
-        : (!firstname || !lastname || !email || !phone) ? AdviseNullFields()
-            : Alert.alert("Add Person", `Value of the fields will be added to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}\nPhone: ${phone}`,
-                [{ text: "Cancel" },
-                {
-                    text: "Save", onPress: () => {
-                        addPerson({ firstname, lastname, email, phone });
-                        navigation.navigate("Home");
-                    }
-                }])
+        : 
+        
+        Alert.alert("Add Person", `Value of the fields will be added to: \n\nFirst Name: ${firstname}\nLast Name: ${lastname}\nEmail: ${email}\nPhone: ${phone}`,
+            [{ text: "Cancel" },
+            {
+                text: "Save", onPress: async () => {
+                    await addPerson({ firstname, lastname, email, phone });
+                    navigation.navigate("Home");
+                }
+            }])
 }
 
 export default handleAddEdit;
