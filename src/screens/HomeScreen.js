68a417dd10263ad690bd14d5e7dba10c4919a7f4
Baumzin <brenosuvale@gmail.com>
2026-03-30 08:52:27 -0300

    feat - Renderização de FlatList + Melhoria da lógica de tratamento de erros

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import { View, Text, Alert } from 'react-native';
-import { useState, useCallback } from 'react';
+import { View, Text, Alert, FlatList } from 'react-native';
+import { useState, useCallback, useEffect } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import Button from '../components/Button';
 import Search from '../components/Search.js';
@@ -14,17 +14,25 @@ const API_URL = process.env.EXPO_PUBLIC_API_URL;
 export default function HomeScreen({ navigation }) {
     const [person, setPerson] = useState([]);
     const [loading, setLoading] = useState(true);
-    // O useEffect foi substituido pelo useFocusEffect para atualizar a lista toda vez que haver uma navegação entre telas, ou seja, quando a tela for focada. 
-    // O useEffect só atualizava a lista quando a tela era carregada pela primeira vez.
+    const [error, setError] = useState(false);
+    const [search, setSearch] = useState("");
+    // useFocusEffect serve para atualizar a lista toda vez que haver uma navegação entre telas, ou seja, quando a tela for focada. 
+
     useFocusEffect(
         useCallback(() => {
             refreshList()
         }, [])
     );
 
+    //useEfffect atualiza a lista apenas na primeira renderização da tela
+    useEffect(() => {
+        refreshList()
+    }, []);
+
     async function refreshList() {
         try {
             setLoading(true);
+            setError(false);
 
             const response = await fetch(`${API_URL}/people`);
             const data = await response.json();
@@ -32,53 +40,74 @@ export default function HomeScreen({ navigation }) {
             setPerson(data);
         }
         catch (error) {
-            alert("Error fetching people. Please try again.");
+            setError(true);
             console.error(error);
         } finally {
             setLoading(false);
         }
     }
 
+    const filteredData = search ?
+        person.filter((item) => {
+            const itemData = `${item.firstname} ${item.lastname} ${item.email} ${item.phone}`.toUpperCase();
+            const textData = search.toUpperCase();
+            return itemData.indexOf(textData) > -1;
+        })
+        :
+        person;
+
     return (
-        <>
-            <View style={styles.container}>
-                {
-                    loading ? (
-                        <Loading />
-                    ) : (
-                        <>
-                            <Search data={person} renderItem={({ item }) => (
-                                <View style={styles.textContainer}>
-                                    <Text style={styles.text}>{item.firstname} {item.lastname}</Text>
-                                    <Text style={styles.text}>{item.email}</Text>
-                                    <Text style={styles.text}>{item.phone}</Text>
-                                    <View style={buttonStyles.buttonContainer}>
-                                        <Button
-                                            title="Delete Person" onPress={() => {
-                                                Alert.alert("Delete Person", "Are you sure you want to delete this person?",
-                                                    [{ text: "Cancel" },
-                                                    {
-                                                        text: "Delete", onPress: () => {
-                                                            deletePerson(item.id).then(() => {
-                                                                refreshList()
-                                                            })
-                                                        }
-                                                    }])
-                                            }}
-                                            backgroundColor={"#f07d7d"} />
-                                        <Button title="Edit Person" onPress={() => navigation.navigate('AddEdit', { person: item })} backgroundColor={"#b3b477"} />
+        <View style={styles.container}>
+            {
+                loading ? (
+                    <Loading />
+                ) : error ? (
+                    <View style={{ alignItems: 'center', padding: 20, margin: 20 }}>
+                        <Text style={{ alignItems: 'center', margin: 10 }}>Erro ao carregar dados</Text>
+                        <Text style={styles.errorMessage}> {error?.message || "Verifique sua conexão com a internet e tente novamente."} </Text>
+                        <Button title="Tentar novamente" onPress={refreshList} backgroundColor={"#79b477"} />
+                    </View>
+                ) : (
+                    <>
+                        <Search search={search} setSearch={setSearch} />
+                        <FlatList
+                            data={filteredData}
+                            keyExtractor={(item) => item.id.toString()}
+                            showsVerticalScrollIndicator={false}
+                            renderItem={({ item }) => (
+                                <View style={styles.listContainer}>
+                                    <View style={styles.textContainer}>
+                                        <Text style={styles.text}>{item.firstname} {item.lastname}</Text>
+                                        <Text style={styles.text}>{item.email}</Text>
+                                        <Text style={styles.text}>{item.phone}</Text>
+                                        <View style={buttonStyles.buttonContainer}>
+                                            <Button
+                                                title="Delete Person" onPress={() => {
+                                                    Alert.alert("Delete Person", "Are you sure you want to delete this person?",
+                                                        [{ text: "Cancel" },
+                                                        {
+                                                            text: "Delete", onPress: () => {
+                                                                deletePerson(item.id).then(() => {
+                                                                    refreshList()
+                                                                })
+                                                            }
+                                                        }])
+                                                }}
+                                                backgroundColor={"#f07d7d"} />
+                                            <Button title="Edit Person" onPress={() => navigation.navigate('AddEdit', { person: item })} backgroundColor={"#b3b477"} />
+                                        </View>
                                     </View>
                                 </View>
-                            )} />
-                            <View style={buttonStyles.buttonContainer}>
-                                <View style={buttonStyles.containerAdd}>
-                                    <Button title="Add Person" onPress={() => navigation.navigate('AddEdit')} backgroundColor={"#79b477"} />
-                                </View>
+                            )}
+                        />
+                        <View style={buttonStyles.buttonContainer}>
+                            <View style={buttonStyles.containerAdd}>
+                                <Button title="Add Person" onPress={() => navigation.navigate('AddEdit')} backgroundColor={"#79b477"} />
                             </View>
-                        </>
-                    )
-                }
-            </View>
-        </>
+                        </View>
+                    </>
+                )
+            }
+        </View>
     );
 }
